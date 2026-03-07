7ba3bf614a449502dc5e9ac3a94ea4b428366707
shameel <muhammadshameelks@gmail.com>
2026-03-07 23:24:49 +0530

    chnaged favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,6 +39,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <title>Scentence | Premium Fragrances</title>
+        <meta name="description" content="Discover a meticulously curated archive of essence. From avant-garde originals to timeless master compositions." />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${cormorantGaramond.variable} ${gayathri.variable} antialiased`}
       >
